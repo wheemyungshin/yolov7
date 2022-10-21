c32c0d343fb539d7fdf21c92896459fdfa9efa45
wheemyungshin <zerger26@gmail.com>
2022-10-21 10:47:36 +0900

    export json results

--- get_ready.sh
+++ get_ready.sh
@@ -9,3 +9,4 @@ pip install seaborn thop
 cd /yolov7
 
 pip install torch==1.7.1+cu101 torchvision==0.8.2+cu101 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
+pip install opencv-python==4.5.5.64
