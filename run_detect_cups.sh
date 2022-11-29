0938a2037cdeeb347b775a57773fbd0b22268d9b
wheemyungshin <zerger26@gmail.com>
2022-11-29 10:05:43 +0900

    run cup data

--- run_detect_cups.sh
+++ run_detect_cups.sh
@@ -3,8 +3,7 @@ numbers=("paper_cups/paper_cups" "plastic_cups/image" "plastic_cups/video")
 subjects=("MIX" "cap" "holder" "normal" "stick" "MIX_video")
 for number in ${numbers[@]}; do
         for subject in ${subjects[@]}; do
-                echo ${target}
-                echo ${target##*/}
-                python3 detect.py --weights weights/yolov7_coco_cups_only_tune_s256.pt --conf 0.25 --source ${target}/${number}/${subject} --name ${target##*/}_${number##*/}_${subject} --save-json
+                echo ${target##*/}_${number##*/}_${subject}
+                python3 detect.py --weights weights/yolov7_coco_and_synthetic_cups_only_tune_s320.pt --conf 0.25 --source ${target}/${number}/${subject} --name ${target##*/}_${number##*/}_${subject} --save-json --save-txt
         done
 done
