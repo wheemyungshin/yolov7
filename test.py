c975b09f5ff1ad5b99a62d0b6663a64ea7f7da74
wheemyungshin <zerger26@gmail.com>
2023-01-11 09:42:49 +0900

    add memryx updated data

--- test.py
+++ test.py
@@ -100,7 +100,6 @@ def test(data,
     seen = 0
     confusion_matrix = ConfusionMatrix(nc=nc)
     names = {k: v for k, v in enumerate(model.names if hasattr(model, 'names') else model.module.names)}
-    print("names:", names)
     coco91class = coco80_to_coco91_class()
     s = ('%20s' + '%12s' * 6) % ('Class', 'Images', 'Labels', 'P', 'R', 'mAP@.5', 'mAP@.5:.95')
     p, r, f1, mp, mr, map50, map, t0, t1 = 0., 0., 0., 0., 0., 0., 0., 0., 0.
