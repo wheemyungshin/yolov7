677a049dca0006e71554dbfb547ef93eb4e6d504
wheemyungshin <zerger26@gmail.com>
2023-02-09 10:22:11 +0900

    update script

--- run_auto_pitta_2nd_divided_sample_clips.sh
+++ run_auto_pitta_2nd_divided_sample_clips.sh
@@ -4,6 +4,6 @@ for dir in $target/*; do
         if [ -d "$dir" ]; then
                 echo $dir
                 echo ${dir##*/}
-                python3 detect.py --weights runs/train/0127memryx_mafa-yolov7-tiny_body_only_s192_3206/weights/epoch_039.pt --conf 0.4 --source $dir --name ${dir##*/} --save-txt --img-size 192 320 --device 0 --agnostic-nms --save-frame
+                python3 detect.py --weights weights/0127memryx_mafa-yolov7-tiny_face_only_s192_320.pt --conf 0.4 --source $dir --name 0127memryx_mafa-yolov7-tiny_face_only_s192_320_${dir##*/} --img-size 192 320 --device 0 --agnostic-nms --save-frame
         fi
 done
