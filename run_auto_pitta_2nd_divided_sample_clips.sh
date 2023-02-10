31035dae04f558f360d57fbed913cae5a66b3aa4
wheemyungshin <zerger26@gmail.com>
2023-02-10 22:27:31 +0900

    crop ciga data

--- run_auto_pitta_2nd_divided_sample_clips.sh
+++ run_auto_pitta_2nd_divided_sample_clips.sh
@@ -4,6 +4,6 @@ for dir in $target/*; do
         if [ -d "$dir" ]; then
                 echo $dir
                 echo ${dir##*/}
-                python3 detect.py --weights weights/tang_ncap_pitta1st_2nd_tune_yolov7-tiny_s192_320.pt  --conf 0.4 --source $dir --name tang_ncap_pitta1st_2nd_tune_yolov7-tiny_s192_320/${dir##*/} --img-size 192 320 --device 0 --agnostic-nms --save-frame
+                python3 detect.py --weights weights/tang_ncap_pitta1st_2nd_smoke_tune_yolov7-tiny_s192_320.pt  --conf 0.4 --source $dir --name tang_ncap_pitta1st_2nd_smoke_tune_yolov7-tiny_s192_320_to_s192_192/${dir##*/} --img-size 192 192 --device 0 --agnostic-nms --save-frame
         fi
 done
