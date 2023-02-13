c58459eb29062c02cbe9b5b0632290e12560cdea
wheemyungshin-nota <wheemyung.shin@nota.ai>
2023-02-13 17:28:03 +0900

    3090a

--- run_auto_pitta_2nd_divided_sample_clips.sh
+++ run_auto_pitta_2nd_divided_sample_clips.sh
@@ -4,6 +4,6 @@ for dir in $target/*; do
         if [ -d "$dir" ]; then
                 echo $dir
                 echo ${dir##*/}
-                python3 detect.py --weights weights/smoke_all_crop_yolov7-tiny_s384_384.pt  --conf 0.4 --source $dir --name smoke_all_crop_yolov7-tiny_s384_384/${dir##*/} --img-size 384 640 --device 0 --agnostic-nms --save-frame
+                python3 detect.py --weights runs/train/smoke_all_crop_tune_yolov7-tiny_boxmargin1_8_s192_1922/weights/epoch_109.pt  --conf 0.4 --source $dir --name smoke_all_crop_tune_yolov7-tiny_boxmargin1_8_s192_192_epoch_109/${dir##*/} --img-size 192 320 --device 0 --agnostic-nms --save-frame
         fi
 done
