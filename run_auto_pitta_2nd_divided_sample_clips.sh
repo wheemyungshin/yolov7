7e63ff9007bff1bdfa1326bc1dc6767169c76ad3
wheemyungshin <zerger26@gmail.com>
2023-02-13 13:40:07 +0900

    truth

--- run_auto_pitta_2nd_divided_sample_clips.sh
+++ run_auto_pitta_2nd_divided_sample_clips.sh
@@ -4,7 +4,6 @@ for dir in $target/*; do
         if [ -d "$dir" ]; then
                 echo $dir
                 echo ${dir##*/}
-                python3 detect.py --weights weights/0127memryx_mafa-yolov7-tiny_face_only_rot_s192_320.pt  --conf 0.4 --source $dir --name 0127memryx_mafa-yolov7-tiny_face_only_rot_s192_320/${dir##*/} --img-size 192 320 --device 0 --agnostic-nms --save-frame
-
+                python3 detect.py --weights weights/0127memryx_mafa-yolov7-tiny_face_only_s192_320.pt --conf 0.4 --source $dir --name 0127memryx_mafa-yolov7-tiny_face_only_s192_320/${dir##*/} --img-size 192 320 --device 0 --agnostic-nms --save-frame
         fi
 done
