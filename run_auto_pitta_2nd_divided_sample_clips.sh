6f02556311b1bcbddad7859e28e7051b9dbca19f
wheemyungshin <zerger26@gmail.com>
2023-03-29 14:56:49 +0900

    add nonbelt

--- run_auto_pitta_2nd_divided_sample_clips.sh
+++ run_auto_pitta_2nd_divided_sample_clips.sh
@@ -4,6 +4,6 @@ for dir in $target/*; do
         if [ -d "$dir" ]; then
                 echo $dir
                 echo ${dir##*/}
-                python3 detect.py --weights weights/smoke_all_crop_yolov7_easyaug_s576_576_c.pt --conf 0.4 --source $dir --name smoke_all_crop_yolov7_easyaug_s576_576_c/${dir##*/} --img-size 576 1024 --device 0 --agnostic-nms --save-frame
+                python3 detect.py --weights weights/seatbelt_crop_yolov7-tiny_s384_384.pt --conf 0.4 --source $dir --name seatbelt_smokeall_bodycrop/${dir##*/} --img-size 384 384 --device 0 --agnostic-nms --save-frame
         fi
 done
