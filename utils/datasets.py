4d089cca321aa0cfab7b4004b228517a2218b6d3
wheemyungshin <zerger26@gmail.com>
2023-07-03 17:22:49 +0900

    0703 sejong

--- utils/datasets.py
+++ utils/datasets.py
@@ -1061,14 +1061,7 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
                                 valid_idx+=1
                         else:
                             valid_idx+=1
-                    
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-                    
-=======
->>>>>>> 00b358df59b3809e9a6fe1535f4e87bdb9bed924
->>>>>>> a7a25b4c74b6c52f798ec4f0664aa33e3849848c
+
                     for idx, ciga_label in enumerate(labels[is_valid_ciga]) :
                         min_ciga_size = 16
                         if min(ciga_label[3]-ciga_label[1], ciga_label[4]-ciga_label[2]) > min_ciga_size and random.random() < 0.5:
