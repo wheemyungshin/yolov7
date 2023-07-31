68f413d4499b6446d5b370341cb6369d2b033275
wheemyungshin <zerger26@gmail.com>
2023-07-31 18:03:24 +0900

    seg bug fix

--- utils/general.py
+++ utils/general.py
@@ -317,15 +317,12 @@ def segments2boxes(segments):
 
 def resample_segments(segments, n=1000):
     # Up-sample an (n,2) segment
-    new_segments = []
     for i, s in enumerate(segments):
         s = np.concatenate((s, s[0:1, :]), axis=0)
         x = np.linspace(0, len(s) - 1, n)
         xp = np.arange(len(s))
-        new_segment = np.concatenate([np.interp(x, xp, s[:, i]) for i in range(2)]).reshape(2, -1).T  # segment xy
-        new_segments.append(new_segment)
-    
-    return new_segment
+        segments[i] = np.concatenate([np.interp(x, xp, s[:, i]) for i in range(2)]).reshape(2, -1).T  # segment xy    
+    return segments
 
 
 def scale_coords(img1_shape, coords, img0_shape, ratio_pad=None):
