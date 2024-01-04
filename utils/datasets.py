6583fc9ae743bfd176e6b2bfe3361fb6d6426013
wheemyung shin <wheemyung.shin@nota.ai>
2024-01-04 21:59:50 +0900

    0104 love

--- utils/datasets.py
+++ utils/datasets.py
@@ -1909,11 +1909,13 @@ def load_image(self, index, ratio_maintain=True):
                     img = cv2.resize(img, (int(w0 * r), int(h0 * r)), interpolation=interp)
             return img, (h0, w0), img.shape[:2]  # img, hw_original, hw_resized
         else:
-            interp = cv2.INTER_AREA if not self.augment else cv2.INTER_LINEAR
             if isinstance(self.img_size, tuple):
-                img = cv2.resize(img, (int(self.img_size[1]), int(self.img_size[0])), interpolation=interp)
+                img = cv2.resize(img, (int(self.img_size[1]), int(self.img_size[0])), interpolation=cv2.INTER_LINEAR)
             else:
-                img = cv2.resize(img, (int(self.img_size), int(self.img_size)), interpolation=interp)
+                r = self.img_size / max(h0, w0)  # resize image to img_size
+                if r != 1:  # always resize down, only resize up if training with augmentation
+                    interp = cv2.INTER_AREA if r < 1 and not self.augment else cv2.INTER_LINEAR
+                    img = cv2.resize(img, (int(w0 * r), int(h0 * r)), interpolation=interp)
             return img, (h0, w0), img.shape[:2]  # img, hw_original, hw_resized
     else:
         return self.imgs[index], self.img_hw0[index], self.img_hw[index]  # img, hw_original, hw_resized
