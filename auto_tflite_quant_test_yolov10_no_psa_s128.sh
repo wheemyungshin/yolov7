d3739bd3d1a233e58d070cc2752231a9499b9f9b
wheemyungshin-nota <wheemyung.shin@nota.ai>
2024-06-17 15:28:06 +0900

    0617

--- auto_tflite_quant_test_yolov10_no_psa_s128.sh
+++ auto_tflite_quant_test_yolov10_no_psa_s128.sh
@@ -1,7 +1,7 @@
 source ~/.bashrc
 conda activate yolov7
 
-target="weights_FI"
+target="weights_XX"
 for w in $target/*; do
         if [[ "$w" == *epoch*.pt ]]; then
             weight=${w##*/}
@@ -10,7 +10,7 @@ for w in $target/*; do
             basename="${weight%.*}"
 
             python export.py --weights ${target}/${basename}.pt --simplify --iou-thres 0.2 --conf-thres 0.01 --img-size 128 128
-            python onnx_head_remover.py --type mobilenet_128 --model-name ${basename}_no_opt_128_128.onnx --dir ${target}
+            python onnx_head_remover.py --type yolov10_no_psa --model-name ${basename}_no_opt_128_128.onnx --dir ${target}
 
             conda deactivate
             conda activate onnx2tf
@@ -22,7 +22,7 @@ for w in $target/*; do
             python tflite_runner2_map_check.py --data data/smoke_all_P_val_n78.yaml \
                 --img 128 --batch 1 --conf 0.05 --iou 0.65 \
                 --weights saved_model/modified_${basename}_no_opt_128_128_integer_quant.tflite \
-                --weights2 ../onnx2tf/saved_model/NMS_mobilenet_s128_128_float32.tflite \
+                --weights2 ../onnx2tf/saved_model/NMS_yolov10_no_psa_s128_128_float32.tflite \
                 --result-txt ${target}
                 
         fi
