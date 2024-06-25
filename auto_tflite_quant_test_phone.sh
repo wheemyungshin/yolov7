33d744e86c005766071cb802bed1492c633f69fc
wheemyungshin-nota <wheemyung.shin@nota.ai>
2024-06-25 16:14:19 +0900

    0625 3090a

--- auto_tflite_quant_test_phone.sh
+++ auto_tflite_quant_test_phone.sh
@@ -15,6 +15,6 @@ onnx2tf -i weights/modified_${basename}_no_opt_128_128.onnx -oiqt
 conda deactivate
 conda activate yolov7
 
-python tflite_runner2_map_check.py --data data/smoke_all_P_val_n78.yaml --img 128 --batch 1 --conf 0.05 --iou 0.65 \
+python tflite_runner2_map_check.py --data data/smoke_all_phone_n78_testset_manual.yaml --img 128 --batch 1 --conf 0.05 --iou 0.65 \
     --weights saved_model/modified_${basename}_no_opt_128_128_integer_quant.tflite \
     --weights2 ../onnx2tf/saved_model/NMS_mobilenet_s128_128_float32.tflite
--- auto_tflite_quant_test_yolov10_no_psa_s128.sh
+++ auto_tflite_quant_test_yolov10_no_psa_s128.sh
@@ -19,7 +19,7 @@ for w in $target/*; do
             conda deactivate
             conda activate yolov7
 
-            python tflite_runner2_map_check.py --data data/smoke_all_P_val_n78.yaml \
+            python tflite_runner2_map_check.py --data data/smoke_all_phone_n78_testset_manual.yaml \
                 --img 128 --batch 1 --conf 0.05 --iou 0.65 \
                 --weights saved_model/modified_${basename}_no_opt_128_128_integer_quant.tflite \
                 --weights2 ../onnx2tf/saved_model/NMS_yolov10_no_psa_s128_128_float32.tflite \
